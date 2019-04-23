91c7c3421566f8f5b43d85c78e4bb2f2174177ab
ArdeshirV <ArdeshirV@protonmail.com>
2019-04-23 22:12:44 +0430

    Add files to repository

---
+++ Run.bat
@@ -0,0 +1,7 @@
+; Run.bat - Run ImgVx86.com to show photos in 'img' directory
+; Copyright 1998-2003 ardeshirv@protonmail.com, Licensed under GPLv3+
+@ECHO OFF
+CLS
+ImgVx86.com img\1.img
+ImgVx86.com img\2.img
+
